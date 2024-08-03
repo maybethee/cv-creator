a9803fdb2b365af322d902b1c639f1f2689538c1
retra <jozalamun@gmail.com>
2024-08-03 09:33:01 -0400

    Display submitted values with an edit button to swap back to the form inputs

--- src/ApplicationWrapper.jsx
+++ src/ApplicationWrapper.jsx
@@ -19,6 +19,7 @@ function ApplicationWrapper() {
   });
 
   const [submittedValues, setSubmittedValues] = useState([]);
+  const [isEditing, setIsEditing] = useState(true);
 
   function handleChange(e) {
     const value = e.target.value;
@@ -34,66 +35,86 @@ function ApplicationWrapper() {
 
     // console.log(state);
 
-    setSubmittedValues((oldValues) => [...oldValues, state]);
+    // const generatedDiv = document.querySelector(".generated-values");
+
+    // generatedDiv.textContent = "";
+
+    // setSubmittedValues((oldValues) => [...oldValues, state]);
+    setSubmittedValues([state]);
+
+    setIsEditing(false);
+  }
+
+  function handleEdit() {
+    setIsEditing(true);
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="page-wrapper">
-        <h1>CV Creator</h1>
-        <div
-          className="cv-wrapper"
-          style={{
-            padding: "3rem",
-            border: "solid 2px black",
-            borderRadius: "12px",
-          }}
-        >
-          <section
-            className="gen-info"
-            style={{
-              border: "solid 1px black",
-              padding: "2rem",
-              marginBottom: "1rem",
-            }}
-          >
-            <GenInfo state={state} handleChange={handleChange} />
-          </section>
-          <section
-            className="education"
-            style={{
-              border: "solid 1px black",
-              padding: "2rem",
-              marginBottom: "1rem",
-            }}
-          >
-            <Education state={state} handleChange={handleChange} />
-          </section>
-          <section
-            className="work"
-            style={{
-              border: "solid 1px black",
-              padding: "2rem",
-              marginBottom: "1rem",
-            }}
-          >
-            <Work state={state} handleChange={handleChange} />
-          </section>
-        </div>
+    <div>
+      <h1>CV Creator</h1>
+      {isEditing ? (
+        <form onSubmit={handleSubmit}>
+          <div className="page-wrapper">
+            <div
+              className="cv-wrapper"
+              style={{
+                padding: "3rem",
+                border: "solid 2px black",
+                borderRadius: "12px",
+              }}
+            >
+              <section
+                className="gen-info"
+                style={{
+                  border: "solid 1px black",
+                  padding: "2rem",
+                  marginBottom: "1rem",
+                }}
+              >
+                <GenInfo state={state} handleChange={handleChange} />
+              </section>
+              <section
+                className="education"
+                style={{
+                  border: "solid 1px black",
+                  padding: "2rem",
+                  marginBottom: "1rem",
+                }}
+              >
+                <Education state={state} handleChange={handleChange} />
+              </section>
+              <section
+                className="work"
+                style={{
+                  border: "solid 1px black",
+                  padding: "2rem",
+                  marginBottom: "1rem",
+                }}
+              >
+                <Work state={state} handleChange={handleChange} />
+              </section>
+              <button type="submit">Submit</button>
+            </div>
+          </div>
+        </form>
+      ) : (
+        <div>
+          <button onClick={handleEdit}>Edit</button>
 
-        <button type="submit">Submit</button>
-
-        {submittedValues.map((values, i) => (
-          <div key={i}>
-            {Object.entries(values).map(([key, value]) => (
-              <p key={key}>
-                {key.charAt(0).toUpperCase() + key.slice(1)}: {value}
-              </p>
+          <div className="generated-values">
+            {submittedValues.map((values, i) => (
+              <div key={i}>
+                {Object.entries(values).map(([key, value]) => (
+                  <p key={key}>
+                    {key.charAt(0).toUpperCase() + key.slice(1)}: {value}
+                  </p>
+                ))}
+              </div>
             ))}
           </div>
-        ))}
-      </div>
-    </form>
+        </div>
+      )}
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import GenInfo from "./GenInfo.jsx";
 import ApplicationWrapper from "./ApplicationWrapper.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
-    <ApplicationWrapper>
-      <GenInfo />
-    </ApplicationWrapper>
+    <ApplicationWrapper />
   </React.StrictMode>
 );
